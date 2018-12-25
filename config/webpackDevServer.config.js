ccb71361b6c449a72b9b5087aa978e6bad37144b
sumruti <46131919+sumruti@users.noreply.github.com>
2018-12-25 23:09:32 +0530

    update webpack config

--- config/webpackDevServer.config.js
+++ config/webpackDevServer.config.js
@@ -9,7 +9,7 @@ const paths = require('./paths');
 const fs = require('fs');
 
 const protocol = process.env.HTTPS === 'true' ? 'https' : 'http';
-const host = process.env.HOST || 'leasefrontend';
+const host = process.env.HOST || '0.0.0.0';
 
 module.exports = function(proxy, allowedHost) {
   return {
